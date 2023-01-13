e817317cbc6144483ce5b64d2ad5cf13ad4beae8
ekoerwiswandoko <ekoerwiswandoko@gmail.com>
2023-01-13 10:20:41 +0700

    live monitoring sterilizer

---
+++ app/Views/Content/LiveMonitoring/SterilizerStation/SterilizerLiveFrameView.php
@@ -0,0 +1,9 @@
+<!-- <div class="col-12 border border-danger" style="<?php  echo $tinggi_dg ; ?>" > -->
+<iframe src="<?php echo $srcView;?>" class="col-12" style="<?php  echo $tinggi_dg ; ?>" >  
+
+</iframe>
+<!-- </div> -->
+
+<script>
+    window.open('<?php echo $srcView;?>', '_blank');
+</script>
